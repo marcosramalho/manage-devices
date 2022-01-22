a085ff1112016cfb475dcf00ccc69af6f93a99ac
Marcos Ramalho <marcos.ramalho.1351@gmail.com>
2022-01-22 20:05:19 -0300

    refactor(api): move duplicated code to a helper method

--- manager-devices-back/src/presentation/controllers/createCategory.controller.ts
+++ manager-devices-back/src/presentation/controllers/createCategory.controller.ts
@@ -1,11 +1,9 @@
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-param-error'
+import { badRequest } from '../helpers/http'
 
 export class CreateCategoryController {
   execute(httpRequest: HttpRequest): HttpResponse {
-    return {
-      statusCode: 400,
-      body: new MissingParamError('name'),
-    }
+    return badRequest(new MissingParamError('name'))
   }
 }
