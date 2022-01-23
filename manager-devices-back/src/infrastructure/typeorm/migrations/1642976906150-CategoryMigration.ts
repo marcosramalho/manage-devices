aa9c03d115d27b4d407d49b1098d5f2f12da9748
Marcos Ramalho <marcos.ramalho.1351@gmail.com>
2022-01-23 19:42:06 -0300

    chore(api): add typeorm and db mysql and generate migrations

---
+++ manager-devices-back/src/infrastructure/typeorm/migrations/1642976906150-CategoryMigration.ts
@@ -0,0 +1,31 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export class CategoryMigration1642976906150 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'categories',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      })
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('categories')
+  }
+}
---
+++ manager-devices-back/src/infrastructure/typeorm/migrations/1642976928400-DeviceMigration.ts
@@ -0,0 +1,53 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm'
+
+export class DeviceMigration1642976928400 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'devices',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+          },
+          {
+            name: 'color',
+            type: 'varchar',
+          },
+          {
+            name: 'partNumber',
+            type: 'int',
+          },
+          {
+            name: 'categoryId',
+            type: 'int',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      })
+    )
+
+    await queryRunner.createForeignKey(
+      'devices',
+      new TableForeignKey({
+        columnNames: ['categoryId'],
+        referencedTableName: 'categories',
+        referencedColumnNames: ['id'],
+      })
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('devices')
+  }
+}
